c7de96bb28b11e9636fc22f02e4c969f7c50d6f1
justinKline24 <justinkline24@gmail.com>
2022-08-02 17:44:15 -0500

    fixed leak

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#postgresql://postgres:password@localhost/grades
-SQLALCHEMY_DATABASE_URL = 'postgresql://justin:aggiegradedata@gradedata.cgvbm0h9cyvd.us-east-2.rds.amazonaws.com/postgres'
+
+SQLALCHEMY_DATABASE_URL = ''
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
