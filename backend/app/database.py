91421ec98255b299d864fa2f0987f679d60a11b4
justinKline24 <justinkline24@gmail.com>
2022-08-02 20:49:36 -0500

    create gitignore

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import creds
 
-SQLALCHEMY_DATABASE_URL = ''
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(creds.SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
