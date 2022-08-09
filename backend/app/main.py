6b2ef68683d68b61f32955fcc7b89565525c3719
Tyler Felton <tylerfelton@tamu.edu>
2022-08-09 12:10:30 -0500

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,5 +9,8 @@ app = FastAPI()
 
 @app.get("/class/{course}-{number}")
 def get_class(number: int,course: str, db: Session = Depends(get_db)):
-    all_classes=db.query(models.Post).filter(models.Post.code==number).filter(models.Post.subject==course).all()
+    #all_classes=db.query(models.Post).filter(models.Post.code==number).filter(models.Post.subject==course).all()
+    #return {"all classes": all_classes}
+    
+    all_classes=db.query(models.Post).filter(models.Post.code==number).filter(models.Post.subject==course).group_by(models.Post.prof)
     return {"all classes": all_classes}
