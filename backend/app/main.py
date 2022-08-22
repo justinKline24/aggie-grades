bd5d2879d7c8795907909799e6651ed24614d6ef
justinKline24 <justinkline24@gmail.com>
2022-08-21 22:09:13 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/class/{course}-{number}")
 def get_class(number: int,course: str, db: Session = Depends(get_db)):
-    result=conn.execute("""SELECT prof, COUNT(*), MAX(year),  SUM(atotal), SUM(atotal), SUM(btotal),SUM(ctotal),SUM(dtotal),SUM(ftotal),SUM(aftotal), SUM(qtotal), CAST(AVG(gpa) AS DECIMAL(3,2)) FROM gradestable\
+    result=conn.execute("""SELECT prof, COUNT(*), MAX(year),  SUM(atotal), SUM(btotal),SUM(ctotal),SUM(dtotal),SUM(ftotal),SUM(aftotal), SUM(qtotal), CAST(AVG(gpa) AS DECIMAL(3,2)) FROM gradestable\
                 WHERE code = %s AND subject= '%s' GROUP BY prof\
                 ORDER BY MAX(year) DESC, AVG(gpa) DESC"""%(str(number),course))
     rows = result.fetchall()
