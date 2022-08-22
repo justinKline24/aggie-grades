7034b99134cdabd283fe0185ce58ab0864bb6944
Tyler Felton <tylerfelton@tamu.edu>
2022-08-22 14:06:26 -0500

    chart query

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,9 +10,13 @@ app = FastAPI()
 
 @app.get("/class/{course}-{number}")
 def get_class(number: int,course: str, db: Session = Depends(get_db)):
-    result=conn.execute("""SELECT prof, COUNT(*), MAX(year),  SUM(atotal), SUM(btotal),SUM(ctotal),SUM(dtotal),SUM(ftotal),SUM(aftotal), SUM(qtotal), CAST(AVG(gpa) AS DECIMAL(3,2)) FROM gradestable\
+    result=conn.execute("""SELECT prof, COUNT(*), MAX(year),  SUM(atotal) AS suma, SUM(btotal),SUM(ctotal),SUM(dtotal),SUM(ftotal),SUM(aftotal), SUM(qtotal), CAST(AVG(gpa) AS DECIMAL(3,2)) FROM gradestable\
                 WHERE code = %s AND subject= '%s' GROUP BY prof\
                 ORDER BY MAX(year) DESC, AVG(gpa) DESC"""%(str(number),course))
     rows = result.fetchall()
+    chart_result = conn.execute("""SELECT prof, gpa, semester,year FROM gradestable\
+                WHERE code = %s AND subject= '%s'\
+                """%(str(number),course))
+    chart_rows = chart_result.fetchall()
     conn.commit()
-    return {"all classes": rows}
+    return {"all classes": rows, "chart return": chart_rows}
